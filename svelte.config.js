89ba32948112de46b1866b50102d7583623a61ad
Furkan Üzümcü <furkanuzumcu@gmail.com>
2025-10-20 15:44:32 +0800

    Pages_1

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// svelte.config.js
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+const isProjectPage = process.env.BASE_PATH && process.env.BASE_PATH !== '';
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+export default {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html' // for SPA routing; or 'index.html'
+    }),
+    paths: {
+      base: process.env.BASE_PATH ?? ''
+    },
+    prerender: { entries: ['*'] }
+  }
 };
-
-export default config;
